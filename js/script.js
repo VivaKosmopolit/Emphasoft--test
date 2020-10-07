dc2d4ce7154532097106effce922b774994e9ca2
VivaKosmopolit <nightspy2015@gmail.com>
2020-10-07 23:38:18 +0300

    Emphasoft Test Release / Fix 2

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ ScrollReveal().reveal('.s-link', { delay: 100, interval: 200, duration: 1000 });
 
 function btn(e) {
     let mobNav = document.querySelector('.mobile-wrapper');
+    let mobWrapper = document.querySelector('.mobile-menu');
     if (e.className == 'burger-wrapper') {
         mobNav.style.transform = 'translateX(0)';
+        mobWrapper.style.position = 'fixed';
+        mobWrapper.style.backgroundColor = '#ffffff';
         e.classList.add('burger-btn_active');
     } else {
         mobNav.style.transform = 'translateX(-100%)';
+        mobWrapper.removeAttribute("style");
         e.classList.remove('burger-btn_active');
     }
 }
